ee92ef6972dc49915ee5ea764e255e11583be76a
Annie Huang <annie.hn@gmail.com>
2020-09-13 16:51:27 +1000

    Verify we can get the data from the NASA after we make their api calls.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-const API_KEY = 'DEMO_KEY'
-const API_URL = `https://api.nasa.gov/insight_weather/?api_key={API_KEY}&feedtype=json&ver=1.0`
+const API_KEY = 'T3xnpBcDG90dkVHCH9j9hFh5ggU8ScnQbEgAHR1s'
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
 // Change this if you need to
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
@@ -8,3 +8,14 @@ const previousWeather = document.querySelector('.previous-weather');
 previousWeatherToggle.addEventListener('click', () => {
   previousWeather.classList.toggle('show-weather');
 })
+
+getWeather();
+
+function getWeather() {
+  fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+}
+
