ff357ed3c9c1f567bd887015ac14208539bf8724
Annie Huang <annie.hn@gmail.com>
2020-09-13 16:34:24 +1000

    Setup API_URL and API_KEY from https://api.nasa.gov/

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-// Where the cool stuff happens
+const API_KEY = 'DEMO_KEY'
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key={API_KEY}&feedtype=json&ver=1.0`
 
 // Change this if you need to
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
